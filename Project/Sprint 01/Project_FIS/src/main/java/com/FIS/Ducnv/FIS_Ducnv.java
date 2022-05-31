11ec2ef7342bb68d84a0b0f42233fe42c6c027e3
ducnv2509 <ducit2509@gmail.com>
2022-05-31 09:52:10 +0700

    add folder

---
+++ Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/FIS_Ducnv.java
@@ -0,0 +1,13 @@
+package com.FIS.Ducnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIS_Ducnv {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FIS_Ducnv.class, args);
+    }
+
+}
