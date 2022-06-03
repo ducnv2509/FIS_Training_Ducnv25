6092f1b4823e207db3bf1126ca97cab7b4211096
ducnv2509 <ducit2509@gmail.com>
2022-06-03 11:59:44 +0700

    sprint 01 v1.2

---
+++ Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/dto/DAO.java
@@ -0,0 +1,12 @@
+package com.fis.ducnv.dto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T> {
+    List<T> getAll();
+    Optional<T> get(Long id);
+    void save(T t);
+    void update(T t);
+    void delete(T t);
+}
--- Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/entities/Person.java
+++ Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/entities/Person.java
@@ -1,9 +1,12 @@
 package com.fis.ducnv.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fis.ducnv.util.DateFormatter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 import java.time.LocalDateTime;
@@ -18,5 +21,17 @@ public class Person extends AbstractEntity {
     private String firstName;
     private String lastName;
     private String password;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateFormatter.DATE_FORMAT)
+    @DateTimeFormat(pattern = DateFormatter.DATE_FORMAT)
+    @Column(nullable = false)
     private LocalDateTime hiringDate;
+
+    public Person(String username, String firstName, String lastName, String password, LocalDateTime hiringDate) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.hiringDate = hiringDate;
+    }
 }
