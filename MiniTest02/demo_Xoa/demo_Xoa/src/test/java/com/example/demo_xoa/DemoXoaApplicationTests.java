0aa00fee093053847cfb73a1c5bafb2190b519b9
ducnv2509 <ducit2509@gmail.com>
2022-06-13 21:46:17 +0700

    up db

---
+++ MiniTest02/demo_Xoa/demo_Xoa/src/test/java/com/example/demo_xoa/DemoXoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_xoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoXoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
