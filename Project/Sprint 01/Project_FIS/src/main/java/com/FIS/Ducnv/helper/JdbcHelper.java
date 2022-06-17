934ea1a7bfc5944e169578bbf42673b3dc002bff
ducnv2509 <ducit2509@gmail.com>
2022-06-17 10:16:01 +0700

    done_sprint2

--- Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/helper/JdbcHelper.java
+++ Project/Sprint 01/Project_FIS/src/main/java/com/FIS/Ducnv/helper/JdbcHelper.java
@@ -1,35 +1,65 @@
 package com.fis.ducnv.helper;
 
+import com.fis.ducnv.util.EnvUtil;
+
 import java.sql.*;
 
 public class JdbcHelper {
-    static String user = "root";
-    static String pass = "123456";
-    static String url = "jdbc:mysql://localhost:3306/Project_FIS?serverTimezone=UTC";
+    static String user = EnvUtil.get("DB_USER");
+    static String pass = EnvUtil.get("DB_PASSWORD");
+    static String url = "jdbc:mysql://" + EnvUtil.get("DB_HOST") + ":" + EnvUtil.get("DB_PORT") + "/" + EnvUtil.get("DB_NAME") + "?serverTimezone=UTC";
     static String driver = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
             Class.forName(driver);
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     public static PreparedStatement getStmt(String sql, Object... args) throws Exception {
-        Connection con = DriverManager.getConnection(url, user, pass);
-        PreparedStatement stmt;
-        if (sql.trim().startsWith("{")) {
-            stmt = con.prepareCall(sql);
-        } else {
-            stmt = con.prepareStatement(sql);
-        }
+        try (Connection con = DriverManager.getConnection(url, user, pass)) {
+            PreparedStatement stmt;
+            if (sql.trim().startsWith("{")) {
+                stmt = con.prepareCall(sql);
+            } else {
+                stmt = con.prepareStatement(sql);
+            }
 
-        for (int i = 0; i < args.length; i++) {
-            stmt.setObject(i + 1, args[i]);
+            for (int i = 0; i < args.length; i++) {
+                stmt.setObject(i + 1, args[i]);
+            }
+            con.close();
+            return stmt;
         }
-        return stmt;
+    }
+
+    static Connection con = null;
+
+    public static Connection getConnection() {
+        try {
+            if (con == null || con.isClosed()) {
+                try {
+                    con = DriverManager.getConnection(url, user, pass);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public void closeConnection() {
+        try {
+            if (con == null && !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static ResultSet query(String sql, Object... args) throws Exception {
@@ -37,19 +67,19 @@ public class JdbcHelper {
         return stmt.executeQuery();
     }
 
-    public static Object value(String sql, Object... args) {
-        try {
-            ResultSet rs = JdbcHelper.query(sql, args);
-            if (rs.next()) {
-                return rs.getObject(0);
-            }
-            rs.getStatement().getConnection().close();
-            return null;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
+//    public static Object value(String sql, Object... args) {
+//        try {
+//            ResultSet rs = JdbcHelper.query(sql, args);
+//            if (rs.next()) {
+//                return rs.getObject(0);
+//            }
+//            rs.getStatement().getConnection().close();
+//            return null;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     public static int update(String sql, Object... args) {
         try {
